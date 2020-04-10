19da40bbe065f8a692b895da98377ce93f12d1b5
rodricks_E <elizabethrodricks@westat.com>
2020-04-10 11:12:20 -0400

    removed unnsessary files

--- footer/landing.php
+++
@@ -1,6 +0,0 @@
-<footer>
-	<div class="footer-wrapper">
-
-		<a href="https://www.westat.com" target="_blank" title="Visit website">
-			<img src="../images/footer_westat_logo.svg" alt="Westat Home page" title="Westat" class="img-wes-logo"></a> <p class="p-wes-title t-white">An Employee-Owned Research Corporation<sup class="t-white">®</sup></p></div>
-	</footer>
